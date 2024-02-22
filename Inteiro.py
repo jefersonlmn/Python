7dab20927123ee96a3d9b516727129199fa2849e
Jeferson Lima <139590809+jefersonlmn@users.noreply.github.com>
2024-02-22 12:06:09 -0300

    Rename ex016.py to Inteiro.py

---
+++ Inteiro.py
@@ -0,0 +1,2 @@
+num = float(input('Digite um valor:'))
+print('O valor {} tem sua parte inteira {:.0f}'.format(num, num))
--- ex016.py
+++
@@ -1,2 +0,0 @@
-num = float(input('Digite um valor:'))
-print('O valor {} tem sua parte inteira {:.0f}'.format(num, num))
